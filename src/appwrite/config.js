a9bb20f958839a9f546aa9770e3002ad396f8889
AniketChile <84616134+AniketChile@users.noreply.github.com>
2025-10-20 04:33:42 +0530

    getSingle Post and getall posts added

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -28,8 +28,54 @@ export class Service {
   }
 
   async updatePost(slug,{title,content,featuredImage,status}){
-    
+    try {
+        return await this.databases.updateDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug,
+            {
+                title,content,featuredImage,status
+            }
+        )     
+    } catch (error) {
+        console.log("Apppwrite service :: updatePost :: error",error);
+    }
   }
+
+  async deletePost(slug){
+    try {
+        await this.databases.deleteDocument(conf.appwriteDatabaseId,conf.appwriteCollectionId,slug);
+        return true;   
+    } catch (error) {
+        console.log("Apppwrite service :: deletePost :: error",error);
+        return false;
+    }
+  }
+
+  async getPost(slug){
+    try {
+      return await this.databases.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      )
+    } catch (error) {
+      console.log("Apppwrite service :: getPost :: error",error);
+    }
+  }
+
+  async getPosts({queries = [Query.equal("status","active")]}){
+    try {
+      return await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        queries
+      )
+    } catch (error) {
+      console.log("Apppwrite service :: getPosts :: error",error);
+    }
+  }
+
 }
 
 const service = new Service();
