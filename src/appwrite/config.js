e923b81a2bb424432e9d35fbfaba6c6cb19b0bb7
AniketChile <84616134+AniketChile@users.noreply.github.com>
2025-10-20 04:41:38 +0530

    filePreview added

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -76,6 +76,37 @@ export class Service {
     }
   }
 
+  //file upload service
+  async uploadFile(file){
+    try {
+      return await this.bucket.createFile(conf.appwriteBucketId,ID.unique(),file);
+    } catch (error) {
+      console.log("Apppwrite service :: uploadFile :: error",error);
+      return false;
+    }
+  }
+
+  //delete file service
+  async deleteFile(fileId){
+    try {
+        await this.bucket.deleteFile(
+        conf.appwriteBucketId,fileId
+      );
+      return true;
+    } catch (error) {
+      console.log("Apppwrite service :: deleteFile :: error",error);
+      return false;
+    }
+  }
+
+  async getFilePreview(fileId){
+    try {
+      return this.bucket.getFilePreview(conf.appwriteBucketId,fileId); 
+    } catch (error) {
+      console.log("Apppwrite service :: getFilePreview :: error",error);
+    }
+  }
+
 }
 
 const service = new Service();
