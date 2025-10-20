397ef42b9955ac5ef41ea42b03d75cbb03c4515f
AniketChile <84616134+AniketChile@users.noreply.github.com>
2025-10-20 07:42:59 +0530

    comps, tailwind installed login & logout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import authService from './appwrite/auth.js'
+import { useDispatch } from 'react-redux';
+import {login,logout} from './store/authSlice.js'
+import { Footer, Header } from './components';
 function App() {
-  console.log(conf)
-  return (
-    <>
-      Hello Yelo
-    </>
-  )
+
+  const [loading,setLoading] = useState(true);
+  const dispatch = useDispatch();
+  
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then(({userData})=>{
+      if(userData){
+        dispatch(login({userData}));
+      }else{
+        dispatch(logout());
+      }
+    })
+    .finally(()=>setLoading(false));
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          Hello
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import reducer from './authSlice.js'
+const store = configureStore({
+    reducer:reducer
+});
+
+export default store;
