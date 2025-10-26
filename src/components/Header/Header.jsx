05586954f15499f164a2db524fefb589dee1d269
AniketChile <84616134+AniketChile@users.noreply.github.com>
2025-10-26 14:39:32 +0530

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import {Footer,Header,Container,Logo,LogoutBtn} from "../index"
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { logout } from '../../store/authSlice';
 
 function Header(props) {
+    const authStatus = useSelector((state)=>state.auth.status)
     return (
         <div>
             Header
@@ -8,4 +13,4 @@ function Header(props) {
     );
 }
 
-export default Header; 
+export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
-
-export {Footer,Header};
+import Container from "./container/Container.jsx";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogOutBtn";
+export {Footer,Header,Container,Logo,LogoutBtn};
