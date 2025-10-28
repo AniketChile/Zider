1ef0ae085f82b86f77bfa0aeabd7b1c8b8d9b80f
AniketChile <84616134+AniketChile@users.noreply.github.com>
2025-10-28 21:10:58 +0530

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,58 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { logout } from '../../store/authSlice';
 
-function Header(props) {
-    const authStatus = useSelector((state)=>state.auth.status)
+function Header() {
+    const authStatus = useSelector((state)=>state.auth.status);
+    const navigate = useNavigate();
+
+    const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Posts",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
+
     return (
-        <div>
-            Header
-        </div>
+       <header className='py-3 shadow bg-gray-500'>
+            <Container>
+               <nav className='flex'>
+                <div className='mr-4'>
+                    <Link to='/'>
+                        <Logo width="70px"/>
+                    </Link>
+                </div>
+                <ul className='flex ml-auto'>
+                    {
+                        navItems.map((item)=>(
+                            
+                        ))
+                    }
+                </ul>
+               </nav>
+            </Container>
+       </header>
     );
-}
+} 
 
 export default Header;
